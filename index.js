7bba13ffe53d2a66b2a2269c25891fb8ea6bf9ff
Andrei Haiducu <andrei.haiducu@midoweb.com>
2019-03-15 13:36:01 +0200

    routes

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const port = 3000
 
 var qr = require('node-qr-image')
 
-app.get('/qr/generate/:code', (req, res) => {
-    res.send(qr.imageSync(req.params.code, { type : 'svg', size: '50', ec_level: 'H'}))
+app.get('/qr/svg/:code', (req, res) => {
+    res.send(qr.imageSync(req.params.code, { type: 'svg', size: '50', ec_level: 'H' }))
+})
+app.get('/qr/png/:code', (req, res) => {
+    res.contentType('image/png');
+    res.send(qr.imageSync(req.params.code))
+})
+app.get('/qr/html/:code', (req, res) => {
+    var base64 = qr.imageSync(req.params.code).toString('base64')
+    res.send(`<p> <img src="data:image/png;base64, ${base64}" style='display:block; width:200px;height:200px;' > </p>`)
 })
 app.listen(port, () => console.log(`QR started ${port}!`))
